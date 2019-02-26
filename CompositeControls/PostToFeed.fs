393b60d7fc7b4c3f56a6ff308a53a8ce072c58fd
Alexandra V <34913405+av2012@users.noreply.github.com>
2019-02-26 14:17:27 +1100

    Added two tests into Post to Feed

--- CompositeControls/PostToFeed.fs
+++ CompositeControls/PostToFeed.fs
@@ -1,14 +1,24 @@
 module PostToFeed
 
 open canopy.classic
+open System
 
-
+let _postToFeedHeader = xpath "//div[contains(text(),'Post to Feed')]"
 let _postTofeed = css "textarea[name='text']"
 let _visibility = css "select[name='object_privacy_view']"
 let _publishAt = css "input[name='date_fake']"
 let _postButton = css "button[type='submit']"
-let _postToFeedElement = "#bx-form-element-text div.show-placeholder > span"
+let _postToFeedElement = css "#bx-form-element-text div.show-placeholder > span"
+
+// see below clickEmojiButton
+let _addEmojiButton = css ".add-emoji a"
+let _joyButton = css ".emoji-items a[title=':joy:']"
+let _addLinkButton = css ".add-link a"
+let _addLink = css "input[name='url']"
+let _addLinkSubmitButton = css "button[name='do_submit']"
+let _addLinkCloseButton = css "button[name='do_cancel']"
 let _postedMessage = css "div.bx-tl-items div.bx-tl-item:first-of-type .bx-tl-item-content p" //"#bx-timeline-main-outline-public div.bx-tl-items div.bx-tl-item:first-of-type .bx-tl-item-content p"
+let _firstPostedLinkFrame = css "div.bx-tl-items div.bx-tl-item:first-of-type .bx-tl-item-content iframe"
 
 
 //unfortunately it is not easy using normal selenium way to insert a text into that control.
@@ -21,3 +31,9 @@ let postMessageAndVerify messageToPost =
     _visibility << "Public"
     click _postButton
     _postedMessage == messageToPost
+
+let clickEmojiButton emojiButton = 
+    click _addEmojiButton
+    let selector = sprintf ".emoji-items a[title='%s']" emojiButton    
+    canopy.classic.waitForElement (css selector)    
+    click (css selector)     
